54fe26414bb20c2fa2eecb004aabeeb80613d6f0
exerussus <solovyov.production@gmail.com>
2024-11-11 01:53:42 +0300

    Auto updated to version 1.0.48

---
+++ Scripts/Extensions/EcsWorldExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Leopotam.EcsLite;
+
+namespace Plugins.Exerussus._1EasyEcs.Scripts.Extensions
+{
+    public static class EcsWorldExtensions
+    {
+        public static EcsPackedEntity NewPackedEntity(this EcsWorld world)
+        {
+            return world.PackEntity(world.NewEntity());
+        }
+    }
+}
