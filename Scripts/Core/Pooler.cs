e879b4ff34e691eebc0d92696df86714d4e4fdc2
exerussus <solovyov.production@gmail.com>
2025-02-02 03:04:53 +0300

    Auto updated to version 1.0.54

--- Scripts/Core/Pooler.cs
+++ Scripts/Core/Pooler.cs
@@ -1,4 +1,5 @@
-﻿using Leopotam.EcsLite;
+﻿using System;
+using Leopotam.EcsLite;
 
 namespace Exerussus._1EasyEcs.Scripts.Core
 {
@@ -9,6 +10,46 @@ namespace Exerussus._1EasyEcs.Scripts.Core
         public ref T Get(int entity);
         public bool Has(int entity);
         public void Del(int entity);
+    }
+    
+    public class PoolerModuleCustom<T> : IPoolerModule<T> where T : struct, IEcsComponent
+    {
+        public PoolerModuleCustom(EcsWorld world, Action<int, EcsPool<T>> onAdd)
+        {
+            _pool = world.GetPool<T>();
+            _onAdd = onAdd;
+        }
+
+        private readonly EcsPool<T> _pool;
+        private readonly Action<int, EcsPool<T>> _onAdd;
+
+        public ref T AddOrGet(int entity)
+        {
+            if (_pool.Has(entity)) return ref _pool.Get(entity);
+            return ref Add(entity);
+        }
+
+        public ref T Add(int entity)
+        {
+            ref var newData = ref _pool.Add(entity);
+            _onAdd.Invoke(entity, _pool);
+            return ref newData;
+        }
+
+        public ref T Get(int entity)
+        {
+            return ref _pool.Get(entity);
+        }
+
+        public bool Has(int entity)
+        {
+            return _pool.Has(entity);
+        }
+
+        public void Del(int entity)
+        {
+            _pool.Del(entity);
+        }
     }
     
     public class PoolerModule<T> : IPoolerModule<T> where T : struct, IEcsComponent
